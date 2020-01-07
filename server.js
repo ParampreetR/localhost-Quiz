d64c3c179f2016f26ab61f3c02bec8d5ba73895e
ParampreetR <raja.rai6789@gmail.com>
2020-01-07 11:01:49 +0530

    Added login page and restricted control center page dummy

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const handlebars = require('express3-handlebars').create({defaultLayout:'main'});
+const authModule = require('./lib/auth.js')
+
+let authIps = []
+
 
 app.engine('handlebars', handlebars.engine);
 app.use(express.static(__dirname + '/static'))
+app.use(require('body-parser')() )
 
 app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT | 3000);
@@ -14,8 +19,47 @@ app.get('/', (req, res) => {
 
 app.get('/quiz', (req,res) => {
 	let name = req.query.name;
+	console.log(name + ' Connected')
 	res.render('quiz', { title:name});
+})
 
+app.get('/login', (req, res) => {
+	res.render('login', { csrf: 'Sample'})
+})
+
+app.post('/auth', (req, res) => {
+	let authStatus = authModule.authRequest(req.body.name, req.body.pass)
+
+	if(req.body.csrf !== 'Sample') authStatus = false
+	if(!authStatus){
+		res.redirect('/login')
+		console.log(req.ip + ' entered wrong login details')
+	}else{
+
+		if(!(req.ip in authIps)) authIps.push(req.ip)
+		res.redirect('/control-center')
+	}
+})
+
+app.get('/control-center', (req, res) => {
+	let authenticated = authIps.some( (ip) => {
+		if(ip == req.ip) return true
+		else false
+	})
+
+	if(authenticated){
+		console.log('Successful login by ' + req.ip)
+		res.render('control-center')
+	}else{
+		console.log(req.ip + ' tried to access ' + req.path + '\n \t Redirected to Login Page')
+		res.redirect('/login')
+	}
+
+})
+
+app.get('/showAuthIps', (req, res) => {
+	res.status(404)
+	console.log(authIps)
 })
 
 app.listen(app.get('port'), () => {
